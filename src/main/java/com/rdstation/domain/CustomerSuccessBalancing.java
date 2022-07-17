f48ce8cabb2df47d67ae268fc52445e0c83e8c51
victorbrugnolo <brugnolovictor@gmail.com>
2022-07-16 21:43:06 -0300

    resolving challenge

--- src/main/java/com/rdstation/domain/CustomerSuccessBalancing.java
+++ src/main/java/com/rdstation/domain/CustomerSuccessBalancing.java
@@ -1,25 +1,63 @@
 package com.rdstation.domain;
 
-import com.rdstation.domain.Customer;
-import com.rdstation.domain.CustomerSuccess;
-import java.util.*;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.OptionalInt;
+import java.util.stream.Collectors;
 
 public class CustomerSuccessBalancing {
 
-    private final List<CustomerSuccess> customerSuccess;
-    private final List<Customer> customers;
-    private final List<Integer> customerSuccessAway;
+  private final List<CustomerSuccess> customerSuccess;
+  private final List<Customer> customers;
+  private final List<Integer> customerSuccessAway;
 
-    public CustomerSuccessBalancing(List<CustomerSuccess> customerSuccess,
-                                    List<Customer> customers,
-                                    List<Integer> customerSuccessAway) {
-        this.customerSuccess = customerSuccess;
-        this.customers = customers;
-        this.customerSuccessAway = customerSuccessAway;
+  public CustomerSuccessBalancing(List<CustomerSuccess> customerSuccess,
+      List<Customer> customers,
+      List<Integer> customerSuccessAway) {
+    this.customerSuccess = customerSuccess;
+    this.customers = customers;
+    this.customerSuccessAway = customerSuccessAway;
+  }
+
+
+  public int run() {
+    int id = 0;
+    long greaterNumberOfCustomersServed = 0;
+
+    List<CustomerSuccess> customerSuccessSorted = sortAndRemoveAlwaysFromCustomerSuccessList();
+
+    for (CustomerSuccess cs : customerSuccessSorted) {
+      HashSet<Customer> customersServed = findCustomersServedByCustomerSuccess(cs.getScore());
+
+      customers.removeAll(customersServed);
+
+      if (customersServed.size() > greaterNumberOfCustomersServed) {
+        id = customersServed.size() == greaterNumberOfCustomersServed ? 0 : cs.getId();
+        greaterNumberOfCustomersServed = customersServed.size();
+      } else {
+        id = customersServed.size() == greaterNumberOfCustomersServed ? 0 : id;
+      }
     }
 
+    return id;
+  }
 
-    public int run() {
-        return 0;
-    }
+  private List<CustomerSuccess> sortAndRemoveAlwaysFromCustomerSuccessList() {
+    OptionalInt minCustomerScoreOptional =
+        customers.stream().mapToInt(Customer::getScore).min();
+    int minCustomerScore = minCustomerScoreOptional.orElse(0);
+    HashSet<Integer> customerSuccessAwayHashSet = new HashSet<>(customerSuccessAway);
+
+    return customerSuccess.stream()
+        .filter(cs -> !customerSuccessAwayHashSet.contains(cs.getId()))
+        .filter(cs -> cs.getScore() >= minCustomerScore)
+        .sorted(Comparator.comparingInt(CustomerSuccess::getScore))
+        .collect(Collectors.toList());
+  }
+
+  private HashSet<Customer> findCustomersServedByCustomerSuccess(int customerSuccessScore) {
+    return customers.stream().filter(customer -> customer.getScore() <= customerSuccessScore)
+        .collect(Collectors.toCollection(HashSet::new));
+  }
 }
